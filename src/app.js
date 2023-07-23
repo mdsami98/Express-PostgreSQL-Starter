d9ef99d06d61793c412b2a5b33f7dc666e9faab9
mdsami9898 <mdsamiwork9898@gmail.com>
2023-07-24 00:23:40 +0600

    structure

--- src/app.js
+++ src/app.js
@@ -2,24 +2,23 @@ const express = require('express');
 const xss = require('xss-clean');
 const config = require('./config/config');
 const cors = require('cors');
-// const corsConfig = require('@/config/cors');
-// import db from '@/database/models';
+const createError = require('http-errors');
+
+// {
+//     origin: '*',
+//     optionsSuccessStatus: 200,
+//     allowedHeaders: ['Content-Type', 'Authorization', 'RefreshToken'],
+//     exposedHeaders: [
+//         'Content-Length',
+//         'Content-Type',
+//         'RefreshToken',
+//         'Token'
+//     ]
+// }
 
 const app = express();
 
-app.use(
-    cors({
-        origin: '*',
-        optionsSuccessStatus: 200,
-        allowedHeaders: ['Content-Type', 'Authorization', 'RefreshToken'],
-        exposedHeaders: [
-            'Content-Length',
-            'Content-Type',
-            'RefreshToken',
-            'Token'
-        ]
-    })
-);
+app.use(cors());
 // parse json request body
 app.use(express.json());
 // parse urlencoded request body
@@ -27,10 +26,17 @@ app.use(express.urlencoded({ extended: true }));
 // sanitize request data
 app.use(xss());
 const db = require('./database/models');
+const createHttpError = require('http-errors');
 app.get('/', async (req, res) => {
     console.log('Congratulations! API is working!');
     res.status(200).send('Congratulations! API is working!');
 });
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
 const server = app.listen(config.port, () => {
     console.log(`Listening to port ${config.port}`);
 });
