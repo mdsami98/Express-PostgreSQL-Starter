d8bbd4c2d85fa6820db3fa59ccf3a232f5ccead5
mdsami9898 <mdsamiwork9898@gmail.com>
2023-07-30 01:15:47 +0600

    add route validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,7 +4,7 @@ const catchAsync = require('@utils/catchAsync');
 const register = catchAsync(async (req, res) => {
     // const user = await userService.createUser(req.body);
     // const tokens = await tokenService.generateAuthTokens(user);
-    res.status(httpStatus.CREATED).send('szdas');
+    res.status(httpStatus.CREATED).send(req.body);
 });
 
 module.exports = {
--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -1,11 +1,15 @@
 const express = require('express');
-// const validate = require('../../middlewares/validate');
-// const authValidation = require('../../validations/auth.validation');
+const validate = require('@middlewares/validate');
+const authValidation = require('@routes/v1/validations/auth.validation');
 const authController = require('@controllers/auth.controller');
 // const auth = require('../../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/register', authController.register);
+router.post(
+    '/register',
+    validate(authValidation.register),
+    authController.register
+);
 
 module.exports = router;
